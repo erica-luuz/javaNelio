a3fa2bf06446211a3715a41b6fd4cd55546dd137
Erica Giselle Francisco Luz <erica.nicollas_hbn@yahoo.com>
2024-11-03 13:24:32 -0300

    Resolução do exercicio 3

---
+++ secao04/Secao4/src/Secao4/Exercicio3.java
@@ -0,0 +1,19 @@
+package Secao4;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+        int D = scan.nextInt();
+
+        int diferenca = (A * B - C*D);
+
+        System.out.println("Diferença = " + diferenca);
+
+    }
+}
